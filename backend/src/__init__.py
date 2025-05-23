86c69ba23e2a13746507545dd0b9fb67e5ce6179
Charles Hayes <cfhayes16@gmail.com>
2025-05-23 17:01:20 -0600

    changes to docker compose and added keycloak

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,19 +1,37 @@
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-api = Api(app)
+# instantiate the extensions
+db = SQLAlchemy()
+admin = Admin(template_mode="bootstrap3")
 
-app.config.from_object('src.config.DevelopmentConfig')
 
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
-class Ping(Resource):
-  def get(self):
-    return {
-      'status': 'success',
-      'message': 'pong!'
-    }
+    # set config
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
 
-api.add_resource(Ping, '/ping')
+    # set up extensions
+    db.init_app(app)
+    if os.getenv("FLASK_ENV") == "development":
+        admin.init_app(app)
 
+    # register api
+    from src.api import api
+
+    api.init_app(app)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
--- backend/src/config.py
+++ backend/src/config.py
@@ -1,14 +1,26 @@
+import os
+
+
 class BaseConfig:
-  TESTING = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "my_precious"
 
 
 class DevelopmentConfig(BaseConfig):
-  pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestingConfig(BaseConfig):
-  TESTING = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 
 class ProductionConfig(BaseConfig):
-  pass
+    url = os.environ.get("DATABASE_URL")
+
+    if url is not None and url.startswith("postgres://"):
+        url = url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = url
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
